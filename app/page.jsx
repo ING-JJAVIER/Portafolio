ec5c4b48cd00fd4b867c9c54af3edd5febaa96ea
Jose Javier <ing.josemartinezmendez@gmail.com>
2024-10-15 08:29:33 -0600

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -3,33 +3,45 @@ export default function Intro() {
   return (
     <>
       <div className="grid font-serif place-content-center h-[918px]">
+        
         <h2 className="font-semibold text-inherit">Hi, We are</h2>
+
         <h1 className="ml-4 text-5xl my-3 text-gray-400">Sigma Developers</h1>
+
         <div className="flex gap-32">
+
           <h2>WEB DEVELOPERS</h2>
+
           <div>
+
             <h2 className="text-lg font-semibold ">Tecnologies</h2>
+
             <ul>
+
               <li className="flex ">
                 <figure><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
                 </svg>
                 </figure>Next JS</li>
+
               <li className="flex ">
                 <figure><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
                 </svg>
                 </figure>Node JS</li>
+
               <li className="flex ">
                 <figure><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
                 </svg>
                 </figure>React</li>
+
               <li className="flex ">
                 <figure><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
                 </svg>
                 </figure>TailWind</li>
+
               <li className="flex ">
                 <figure><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
@@ -40,11 +52,14 @@ export default function Intro() {
                   <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
                 </svg>
                 </figure>JavaScript</li>
-            </ul>
-          </div>
-        </div>
-      </div>
 
+            </ul>
+
+          </div>
+
+        </div>
+
+      </div>
     </>
   )
 }
